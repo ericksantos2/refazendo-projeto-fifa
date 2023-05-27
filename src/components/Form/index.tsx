2ecf1e116f9e79862dd3f7af49bc8f247739764d
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-27 14:43:22 -0300

    deixei o componente form mais genérico para poder ser reutilizado

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,53 +1,92 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+} from '@mui/material';
 import { FormDiv } from './styled';
-import Botao from '../Botao/styled';
-import inputsForm from './inputs';
-import Input from '../Input';
-import Select from '../Select';
-import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { ChangeEvent, useState } from 'react';
+import { inputForm } from '../../interfaces/inputs';
 
-type opcao = 'qualidade' | 'liga' | 'clube' | 'nacionalidade';
-export type ResultadoForm = {
-  [texto in opcao]: string;
-};
+export type TypeValues = { [x: string]: { value: string; text: string } };
 
-export default function Form() {
-  const { register, handleSubmit, setValue } = useForm<ResultadoForm>();
+interface Props {
+  inputs: inputForm[];
+  handleChange: (resultados: TypeValues) => void;
+}
 
-  const onSubmit: SubmitHandler<ResultadoForm> = (data) => {
-    console.log(data);
-  };
+export default function Form({ inputs, handleChange: onChange }: Props) {
+  const [values, setValues] = useState<TypeValues>({});
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    onChange(values);
+  }
+
+  const handleChange =
+    (value: string) =>
+    (
+      e: SelectChangeEvent | ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+      const inputFind = inputs.find((input) =>
+        input.inputs?.some((opcao) => opcao.value === e.target.value)
+      );
+      const opcao = inputFind?.inputs?.find(
+        (input) => input.value === e.target.value
+      );
+      setValues((currentValues) => ({
+        ...currentValues,
+        [value]: {
+          value: e.target.value,
+          text: opcao?.text || '',
+        },
+      }));
+    };
 
   return (
     <FormDiv>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {inputsForm.map((inputForm, index) => (
-          <React.Fragment key={index}>
-            {inputForm.type === 'input' ? (
-              <Input
-                {...register(inputForm.value as opcao, {
-                  required: inputForm.required || false,
-                })}
-                label={inputForm.label}
-                key={index}
-              />
-            ) : (
-              <Select
-                label={inputForm.label}
-                setValue={setValue}
-                value={inputForm.value}
-                key={index}
-              >
-                {inputForm.inputs?.map((input, indexInput) => (
-                  <option value={input.value} key={indexInput}>
-                    {input.text}
-                  </option>
-                ))}
-              </Select>
-            )}
-          </React.Fragment>
-        ))}
-        <Botao type='submit'>Enviar</Botao>
+      <form onSubmit={handleSubmit}>
+        {inputs.map((input, index) => {
+          const id = uuidv4();
+          return (
+            <FormControl key={index} sx={{ minWidth: '250px' }}>
+              {input.type === 'select' && (
+                <>
+                  <InputLabel id={id}>{input.label}</InputLabel>
+                  <Select
+                    labelId={id}
+                    label={input.label}
+                    value={values[input.value]?.value || ''}
+                    onChange={handleChange(input.value)}
+                  >
+                    {input.inputs?.map((opcao, indexB) => (
+                      <MenuItem value={opcao.value} key={indexB}>
+                        {opcao.text}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </>
+              )}
+              {input.type === 'input' && (
+                <>
+                  <TextField
+                    id={id}
+                    label={input.label}
+                    variant='outlined'
+                    onChange={handleChange(input.value)}
+                  />
+                </>
+              )}
+            </FormControl>
+          );
+        })}
+        <Button variant='contained' type='submit'>
+          Enviar
+        </Button>
       </form>
     </FormDiv>
   );
