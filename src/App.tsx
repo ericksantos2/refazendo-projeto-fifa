0a3c8d0d4780bd4c097f426790eeec9fc431a95d
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-27 14:19:18 -0300

    Substitui o componente de form que era usado aqui pelo componente de FormNovoJogador

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Form from "./components/Form";
+import FormNovoJogador from "./components/FormNovoJogador";
 import AppContainer, { AppRoot } from "./styles/AppStyled";
 
 export default function App() {
   return (
     <AppRoot>
       <AppContainer>
-        <Form />
+        <FormNovoJogador />
       </AppContainer>
     </AppRoot>
   )
