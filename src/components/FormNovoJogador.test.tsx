1799344f1a8e5b6defc0d8c34824ae8daa580de1
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-27 22:47:16 -0300

    fiz o teste de submit do form (não dá pra selecionar algo no select pois as opções não aparecem para o jest)

---
+++ src/components/FormNovoJogador.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react';
+import Form from './Form';
+import { inputForm } from '../interfaces/inputs';
+
+describe('Testes do formulário', () => {
+  test('Teste submit', () => {
+    const handleSubmit = jest.fn();
+    const inputs: inputForm[] = [
+      {
+        label: 'Qualidade',
+        type: 'select',
+        value: 'qualidade',
+      },
+      {
+        label: 'Liga',
+        type: 'select',
+        value: 'liga',
+      },
+    ];
+  
+    render(
+      <>
+        <Form inputs={inputs} handleSubmit={handleSubmit} />
+      </>
+    );
+    const botaoEnviar = screen.getByText('Enviar');
+    botaoEnviar.click();
+
+    const expectedOutput = inputs.reduce(
+      (state, { value }) => ({ ...state, [value]: {} }),
+      {}
+    );
+
+    console.log(handleSubmit.mock.calls);
+
+    expect(handleSubmit).toHaveBeenCalledWith(expectedOutput);
+  });
+});
