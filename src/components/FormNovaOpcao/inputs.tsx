54323cad753d352cfcbc6ed357df687fc0f652de
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-28 23:26:37 -0300

    adicionei os inputs do novo componente

---
+++ src/components/FormNovaOpcao/inputs.tsx
@@ -0,0 +1,26 @@
+import { inputForm } from "../../interfaces/inputs";
+
+const inputsForm: inputForm[] = [
+  {
+    type: 'input',
+    value: 'qualidade',
+    label: 'Qualidade'
+  },
+  {
+    type: 'input',
+    value: 'liga',
+    label: 'Liga',
+  },
+  {
+    type: 'input',
+    value: 'clube',
+    label: 'Clube',
+  },
+  {
+    type: 'input',
+    value: 'nacionalidade',
+    label: 'Nacionalidade',
+  },
+];
+
+export default inputsForm;
