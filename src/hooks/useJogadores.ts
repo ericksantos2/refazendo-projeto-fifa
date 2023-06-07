c819889116fd23bc0f99f7291724fbfaba2f77cb
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-07 13:57:57 -0300

    adicionei um reducer para conseguir estatisticas, mas ainda falta conseguir o item com maior quantidade de cada array

--- src/hooks/useJogadores.ts
+++ src/hooks/useJogadores.ts
@@ -1,18 +1,63 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { adicionarJogador } from '../store/reducers/jogadores';
-import { IJogador } from '../interfaces/jogador';
+import { IJogador, ItemJogador } from '../interfaces/jogador';
 import { RootState } from '../store';
+
+type KeysEstatisticas = 'qualidade' | 'liga' | 'clube' | 'nacionalidade';
+
+interface ItemEstatisticas {
+  nome: string;
+  value: string;
+  quantidade: number;
+}
+
+type Estatisticas = {
+  [x in KeysEstatisticas]: ItemEstatisticas[];
+}
 
 export default function useJogadores() {
   const dispatch = useDispatch();
   const jogadores = useSelector((state: RootState) => state.jogadoresSlice);
   return {
     criaJogador(jogadorProps: IJogador) {
-      const jogador = {...jogadorProps};
-      dispatch(adicionarJogador(jogador))
+      const jogador = { ...jogadorProps };
+      dispatch(adicionarJogador(jogador));
     },
     listaJogadores() {
       return jogadores;
-    }
-  }
+    },
+    getEstatisticas() {
+      const estatisticas = jogadores.reduce((state, action) => {
+        return (Object.entries(action) as [KeysEstatisticas, ItemJogador][]).reduce(
+          (stateEntries, [typeNaoTipado, { text, value }]) => {
+            const type = typeNaoTipado as KeysEstatisticas
+            const retornoB: Estatisticas = { ...stateEntries, [type]: [] };
+            stateEntries[type] !== undefined && (retornoB[type] = [...stateEntries[type]]);
+            const index = stateEntries[type]?.findIndex(
+              (item) => item.value === value
+            ) as number;
+            if (stateEntries && stateEntries[type] && index >= 0) {
+              (retornoB[type] as ItemEstatisticas[])[index].quantidade++;
+            } else {
+              retornoB[type].push({
+                value,
+                quantidade: 1,
+                nome: text,
+              });
+            }
+            return retornoB;
+          },
+          { ...state }
+        );
+      }, {} as Estatisticas);
+      console.log(estatisticas);
+      /*
+        {
+          qualidade: [{ value: 'ouro', quantidade: 1, nome: 'Ouro'}],
+          liga: [{ value: 'la_liga', quantidade: 1, nome: 'La Liga'}],
+        }
+      */
+      return estatisticas;
+    },
+  };
 }
