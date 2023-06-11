8f6a3e1c68084269ec92fcc437fdebe89c2a83aa
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-11 20:23:38 -0300

    fiz um teste para ver se os inputs estão funcionando corretamente

--- src/components/FormNovoJogador.test.tsx
+++ src/components/FormNovoJogador.test.tsx
@@ -2,22 +2,42 @@ import { render, screen } from '@testing-library/react';
 import Form from './Form';
 import { inputForm } from '../interfaces/inputs';
 
+const inputs: inputForm[] = [
+  {
+    label: 'Qualidade',
+    type: 'select',
+    value: 'qualidade',
+  },
+  {
+    label: 'Liga',
+    type: 'select',
+    value: 'liga',
+  },
+];
+
 describe('Testes do formulário', () => {
+  test('Teste se os inputs estão certos', async () => {
+    const handleSubmit = jest.fn();
+    render(
+      <>
+        <Form inputs={inputs} handleSubmit={handleSubmit} />
+      </>
+    );
+    ['Qualidade', 'Liga']
+      .map((item) => screen.getByLabelText(item))
+      .forEach((item) => expect(item).toBeDefined());
+    const inputNaoExistente = (() => {
+      try {
+        return screen.getByLabelText('Clube');
+      } catch {
+        return undefined;
+      }
+    })();
+    expect(inputNaoExistente).toBeUndefined();
+  });
+
   test('Teste submit', () => {
     const handleSubmit = jest.fn();
-    const inputs: inputForm[] = [
-      {
-        label: 'Qualidade',
-        type: 'select',
-        value: 'qualidade',
-      },
-      {
-        label: 'Liga',
-        type: 'select',
-        value: 'liga',
-      },
-    ];
-  
     render(
       <>
         <Form inputs={inputs} handleSubmit={handleSubmit} />
