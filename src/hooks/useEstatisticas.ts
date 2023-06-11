fe45b504a623654382b557765311f1bd23fefc49
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-10 22:28:25 -0300

    fiz um hook para estatisticas

---
+++ src/hooks/useEstatisticas.ts
@@ -0,0 +1,48 @@
+import { Estatisticas, ItemEstatisticas, KeysEstatisticas, Maiores } from '../interfaces/estatisticas';
+import { ItemJogador } from '../interfaces/jogador';
+import useJogadores from './useJogadores';
+
+export default function useEstatisticas() {
+  const jogadores = useJogadores().lista;
+  const estatisticas = jogadores.reduce((state, action) => {
+    return (Object.entries(action) as [KeysEstatisticas, ItemJogador][]).reduce(
+      (stateEntries, [typeNaoTipado, { text, value }]) => {
+        const type = typeNaoTipado as KeysEstatisticas;
+        const retornoB: Estatisticas = { ...stateEntries, [type]: [] };
+        stateEntries[type] !== undefined &&
+          (retornoB[type] = [...stateEntries[type]]);
+        const index = stateEntries[type]?.findIndex(
+          (item) => item.value === value
+        ) as number;
+        if (stateEntries && stateEntries[type] && index >= 0) {
+          (retornoB[type] as ItemEstatisticas[])[index].quantidade++;
+        } else {
+          retornoB[type].push({
+            value,
+            quantidade: 1,
+            nome: text,
+          });
+        }
+        return retornoB;
+      },
+      { ...state }
+    );
+  }, {} as Estatisticas);
+  const maiores = Object.entries(estatisticas).reduce(
+    (state, [keyNaoTipada, item]: [string, ItemEstatisticas[]]) => {
+      const key = keyNaoTipada as KeysEstatisticas;
+      const itemASerRetornado = item.sort(
+        (a, b) => b.quantidade - a.quantidade
+      )[0];
+      return {
+        ...state,
+        [key]: itemASerRetornado,
+      };
+    },
+    {} as Maiores
+  );
+  return {
+    estatisticas,
+    maiores,
+  };
+}
